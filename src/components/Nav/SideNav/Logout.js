83919959336f2449372e31df4c1d2ba08349797a
Nitin Pal <77119172+nitinpal2001@users.noreply.github.com>
2022-01-06 00:34:34 +0530

    logout in Sidenav created

---
+++ src/components/Nav/SideNav/Logout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import { List, Divider, useMediaQuery, useTheme } from "@material-ui/core";
+import { Home as HomeIcon, Whatshot as TrendingIcon } from "@material-ui/icons";
+import NavItem from "../NavItem";
+import { toggleDrawer } from "../../../redux/actions/layout";
+
+function Logout() {
+    const theme = useTheme();
+  
+    const isMinScreenMd = useMediaQuery(theme.breakpoints.up("md"));
+    const dispatch = useDispatch();
+    const handleItemClick = () => {
+      if (!isMinScreenMd) {
+        dispatch(toggleDrawer(isMinScreenMd));
+      }
+    };
+  
+    return (
+      <List style={{position:"absolute",bottom:"10px",width:"100%"}}>
+        {[
+          {
+            title: "Logout",
+            icon: TrendingIcon,
+            path: "/trending",
+          }
+        ].map((item, index) => {
+          return (
+            <React.Fragment key={index}>
+              <NavItem
+                to={item.path}
+                title={item.title}
+                icon={item.icon}
+                onClick={handleItemClick}
+              />
+            </React.Fragment>
+          );
+        })}
+      </List>
+    );
+  };
+
+export default Logout
