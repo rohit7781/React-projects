71d1aca2403adf66c98544fdbbbf27524a826a53
Mandeepsingh3030 <mandeepsinghworkspace@gmail.com>
2022-02-12 00:17:27 +0530

    metamask wallet integration

--- src/components/Nav/TopNav/EndNav.js
+++ src/components/Nav/TopNav/EndNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { Toolbar, IconButton, Hidden, Tooltip } from "@material-ui/core";
@@ -12,6 +12,7 @@ import {ReactComponent as ConnectWallet} from '../../../assets/connect-wallet.sv
 import { setMobileSearch } from "../../../redux/actions/layout";
 import NavUserMenuBtn from "./NavUserMenuBtn";
 import NavVidMenuBtn from "./NavVidMenuBtn";
+import MetaMaskOnboarding from "@metamask/onboarding"
 import { svg } from "caniuse-lite/data/features";
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -20,9 +21,19 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor:"black",
     color:"white",
   },
+  walletBtn: {
+   padding: "0.2rem",
+   width: "23px",
+   height: "25px"
+  },
+  walletText: {
+    margin: "10px 1px",
+    fontSize: "smaller",
+    cursor: "pointer",
+  },
   iconButton: {
     backgroundColor: "black",
-    color:"white",    
+    color:"white",
     "&:hover": {
       //you want this to be the same as the backgroundColor above
       backgroundColor: "black",
@@ -35,12 +46,52 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-
 const NavBar = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector(({ channel }) => channel.isAuth);
   const classes = useStyles();
   const theme = useTheme();
+  const [walletStatus,setWalletStatus] = useState("CONNECT WALLET")
+  const [accountAddress, setAccountAddress] = useState("");
+  const onboarding = new MetaMaskOnboarding();
+  const isMetaMaskInstalled = () => {
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  useEffect(() => {
+    if (accountAddress === "") {
+    isMetaMaskInstalled() ? setWalletStatus("CONNECT WALLET") : setWalletStatus("Install Metamask") 
+  } else {
+    setWalletStatus("CONNECTED")
+  }
+  },[accountAddress])
+
+  const installMetaMask = () => {
+    onboarding.startOnboarding();
+  }
+
+  async function getAccount() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = accounts[0];
+    return account;
+  }
+
+  const walletConnect = () => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      getAccount().then((response) => {
+        setAccountAddress(response);
+      });
+    } else {
+      console.log("error");
+    }
+  };
+  
   return (
     <Toolbar
       classes={{
@@ -70,10 +121,12 @@ const NavBar = () => {
           </Tooltip>
         )}
         </Hidden>
-        <Tooltip title="Connect Wallet" >
+        <Tooltip title={`${walletStatus}`} >
           <div style={{display:"flex",alignItems:"center",cursor:"pointer",border:"1px solid white",borderRadius: "5px",padding:"2px 2px"}}>
-            <ConnectWallet style={{width:"25px",height:"23px"}}/>
-            <p style={{margin:"10px 1px",fontSize:"smaller"}}>CONNECT WALLET</p>
+            <ConnectWallet className={classes.walletBtn}/>
+            <p className={classes.walletText} onClick={()=> {
+              walletStatus === "Install Metamask" ? installMetaMask() : walletConnect()
+            }}>{walletStatus}</p>
           </div>
         </Tooltip>
         {isAuth && <NavUserMenuBtn />}
