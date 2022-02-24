f8916a3b6276a270ae65a206e4caff6216891dc9
Mandeepsingh3030 <mandeepsinghworkspace@gmail.com>
2022-02-24 17:59:13 +0530

    Signin button added

--- src/components/Nav/TopNav/EndNav.js
+++ src/components/Nav/TopNav/EndNav.js
@@ -1,7 +1,10 @@
 import React,{useState, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSignInAlt}from '@fortawesome/free-solid-svg-icons'
+import Button from "@material-ui/core/Button";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { Toolbar, IconButton, Hidden, Tooltip } from "@material-ui/core";
+import { Toolbar, IconButton, Hidden, Tooltip} from "@material-ui/core";
 import {
   Search as SearchIcon,
   MoreVert as MoreIcon,
@@ -14,7 +17,16 @@ import NavUserMenuBtn from "./NavUserMenuBtn";
 import NavVidMenuBtn from "./NavVidMenuBtn";
 import MetaMaskOnboarding from "@metamask/onboarding"
 import { svg } from "caniuse-lite/data/features";
+import cssLetterSpacing from "caniuse-lite/data/features/css-letter-spacing";
 const useStyles = makeStyles((theme) => ({
+  signButton:{
+    color: "white",
+    border: "0.5px solid white",
+    margin: "1rem 1rem",
+    padding: "0.5rem",
+    fontWeight: "500",
+    fontSize: "smaller",
+  },
   toolbar: {
     paddingLeft: "0px",
     paddingRight:"9px",
@@ -30,6 +42,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "10px 1px",
     fontSize: "smaller",
     cursor: "pointer",
+    fontWeight: "500",
   },
   iconButton: {
     backgroundColor: "black",
@@ -41,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
     },
     "&:focus": {
       outline: "white",
-    }
+    },
   }
 }));
 
@@ -122,6 +135,13 @@ const NavBar = () => {
           </Tooltip>
         )}
         </Hidden>
+
+        <Tooltip title="Signin">
+        <Button className= {classes.signButton}>
+        <div style = {{marginRight: "7.5px", fontSize: "14px"}}>
+        <FontAwesomeIcon icon={faSignInAlt}></FontAwesomeIcon></div>
+        Sign up</Button>
+        </Tooltip>
         <Tooltip title={`${walletStatus}`} >
           <div style={{display:"flex",alignItems:"center",cursor:"pointer",border:"1px solid white",borderRadius: "5px",padding:"2px 2px"}}>
             <ConnectWallet className={classes.walletBtn}/>
